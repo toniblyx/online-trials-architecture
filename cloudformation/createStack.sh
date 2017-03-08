5be627fa57393089180d4fbd3d1b0bf3ff900cfd
Martin Whittington <martin.whittington@alfresco.com>
2017-03-08 15:44:09 +0000

    TAA-799 - Creating of SNS and Lambda. Some pipeline work.

---
+++ cloudformation/createStack.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# This script will create a copy of the online trial architecture, testing
+# that the template can be created from scratch.
+# Bamboo variables needed are:
+# Aws Access access key ${bamboo.aws_access_key_id}
+# Secret access key ${bamboo.secret_access_password}
+# Stack name ${bamboo.stackName}-${bamboo.planRepository.branchName}-${bamboo.buildNumber}
+
+usage() {
+  echo "Usage: createStack.sh <access_key_id> <secret_access_key> <stack_name>"
+  exit 1
+}
+
+if [ $# -lt 3 ]; then
+  usage
+else
+  source common.func
+  TEMPLATE_NAME="online-trial-control.yaml"
+  S3_TEMPLATE_NAME="$3.yaml"
+  STACK_NAME=$(echo -n $3 | tr / - | awk '{print tolower($0)}')
+  CAPABILITIES=CAPABILITY_IAM
+  STACK_CREATION_TIMEOUT=600
+
+  # Essential Variables
+  export AWS_ACCESS_KEY_ID=$1
+  export AWS_SECRET_ACCESS_KEY=$2
+
+  # Some logging
+  printVarSummary
+
+  # Copy the template to S3
+  separator
+  logInfo "Copy Cloudformation Template $TEMPLATE_NAME to S3 Bucket s3://$S3_BUCKET/"
+  aws s3 mb s3://$S3_BUCKET
+  aws s3 cp $TEMPLATE_NAME s3://$S3_BUCKET/$S3_TEMPLATE_NAME
+
+  # Validate the template
+  separator
+  logInfo "Validate the Cloudformation template"
+  aws cloudformation validate-template --template-url https://s3.amazonaws.com/$S3_BUCKET/$S3_TEMPLATE_NAME
+
+  # Create the stack
+  separator
+  aws cloudformation create-stack --stack-name $STACK_NAME --template-url https://s3.amazonaws.com/$S3_BUCKET/$S3_TEMPLATE_NAME --capabilities $CAPABILITIES --disable-rollback
+  if [ $? -ne 0 ]; then
+    logError "Stack creation failed."
+  fi
+
+  # Wait for the stack to create OK/FAIL
+  separator
+  logInfo "Waiting for stack creation - $STACK_NAME"
+
+  CREATION_COMPLETE_EVENT="FALSE"
+  LOOP_COUNTER=0
+  while [ "$CREATION_COMPLETE_EVENT" != "CREATE_COMPLETE" ]
+  do
+      if [ $LOOP_COUNTER -eq $STACK_CREATION_TIMEOUT ]; then
+          TIMEOUT_IN_MIN=`expr $STACK_CREATION_TIMEOUT / 6`
+          logError "Stack creation timeout after $TIMEOUT_IN_MIN minutes"
+      fi
+      STATUS=`aws cloudformation describe-stack-events --stack-name $STACK_NAME | head -n 10 | grep -B1 AWS::CloudFormation::Stack`
+      monitorStatus "$STATUS"
+      sleep 10
+      LOOP_COUNTER=`expr $LOOP_COUNTER + 1`
+  done
+fi
---
+++ cloudformation/updateStack.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+STACK_UPDATE_TIMEOUT=400
+CAPABILITIES=CAPABILITY_IAM
+
+usage() {
+  echo "Usage: updateStack.sh <access_key_id> <secret_access_key> <stack_name>"
+  exit 1
+}
+
+source common.func
+# Essential Variables
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+STACK_NAME=$(echo -n $3 | tr / - | awk '{print tolower($0)}')
+
+# We need to create a change set for the current stack, describe the change set and check the response for the "STATUS"
+# if the status was "FAILED" this was because there we no changes to execute, so we delete the change set then exit early
+# otherwise we execute the change set
+S3_TEMPLATE_NAME="$3.yaml"
+aws cloudformation create-change-set --stack-name $STACK_NAME --template-url https://s3.amazonaws.com/$S3_BUCKET/$S3_TEMPLATE_NAME --change-set-name trial-update --capabilities $CAPABILITIES
+# give the changeset time to be created
+separator
+logInfo "Waiting for the change set to be created...."
+sleep 30
+RESULT=$(aws cloudformation describe-change-set --stack-name $STACK_NAME --change-set-name trial-update --query "Status" --output text)
+if [ "$RESULT" == "FAILED" ]; then
+  separator
+  logInfo "No updates to execute on $STACK_NAME. Exiting"
+  aws cloudformation delete-change-set --stack-name $STACK_NAME --change-set-name trial-update
+  exit 0
+fi
+
+separator
+logInfo "Updating stack $STACK_NAME"
+aws cloudformation execute-change-set --change-set-name trial-update --stack-name $STACK_NAME
+if [ $? -ne 0 ]; then
+  logError "Stack update failed."
+fi
+
+separator
+logInfo "Waiting for stack update $STACK_NAME"
+UPDATE_COMPLETE_EVENT="FALSE"
+LOOP_COUNTER=0
+while [ "$UPDATE_COMPLETE_EVENT" != "UPDATE_COMPLETE" ]
+do
+    if [ $LOOP_COUNTER -eq $STACK_UPDATE_TIMEOUT ]; then
+        TIMEOUT_IN_MIN=`expr $STACK_UPDATE_TIMEOUT / 6`
+        logError "Stack update timeout after $TIMEOUT_IN_MIN minutes"
+    fi
+
+    STATUS=`aws cloudformation describe-stack-events --stack-name $STACK_NAME | head -n 10 | grep -B1 AWS::CloudFormation::Stack`
+    monitorStatus "$STATUS"
+    sleep 10
+    LOOP_COUNTER=`expr $LOOP_COUNTER + 1`
+done
